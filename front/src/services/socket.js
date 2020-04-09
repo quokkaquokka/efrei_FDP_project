82fb3fdbb8faf741d8704f5b7d897924ff685cc4
jsaget <jonathan.saget@gmail.com>
2020-04-09 18:47:12 +0200

    Display last violation and interventions. Add intervention demand

--- front/src/services/socket.js
+++ front/src/services/socket.js
@@ -13,7 +13,7 @@ export default class Socket {
 
     this.socket.on('connect', function(message) {
       console.log('SIO: connect', message)
-  })
+    })
 
     this.socket.on('welcome', function(message) {
         console.log('SIO: Welcome', message)
@@ -23,6 +23,47 @@ export default class Socket {
       console.log('SIO: msg', message)
       // do something (transform) with the message
       this.eventAggregator.publish('msg', message);
+    })
+
+
+
+    this.socket.on('newIntervention', message => {
+      console.log('SIO: newIntervention', message)
+      if (typeof message === 'string') message = JSON.parse(message)
+      // do something (transform) with the message
+      this.eventAggregator.publish('newIntervention', message);
+    })
+    this.socket.on('disableIntervention', message => {
+      console.log('SIO: disableIntervention', message)
+      if (typeof message === 'string') message = JSON.parse(message)
+      // do something (transform) with the message
+      this.eventAggregator.publish('disableIntervention', message);
+    })
+    this.socket.on('newViolation', message => {
+      console.log('SIO: newViolation', message)
+      if (typeof message === 'string') message = JSON.parse(message)
+      // do something (transform) with the message
+      this.eventAggregator.publish('newViolation', message);
+    })
+    this.socket.on('take-intervention-refused', message => {
+      console.log('SIO: take-intervention-refused', message)
+      if (typeof message === 'string') message = JSON.parse(message)
+      // do something (transform) with the message
+      this.eventAggregator.publish('take-intervention-refused', message);
+    })
+
+    this.socket.on('take-intervention-accepted', message => {
+      console.log('SIO: take-intervention-accepted', message)
+      if (typeof message === 'string') message = JSON.parse(message)
+      // do something (transform) with the message
+      this.eventAggregator.publish('take-intervention-accepted', message);
+    })
+
+    this.socket.on('remove-intervention', message => {
+      console.log('SIO: remove-intervention', message)
+      if (typeof message === 'string') message = JSON.parse(message)
+      // do something (transform) with the message
+      this.eventAggregator.publish('remove-intervention', message);
     })
   }
 
--- front/src/welcome.js
+++ front/src/welcome.js
@@ -1,6 +1,8 @@
 import { inject } from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import Socket from './services/socket';
+import axios from "axios";
+import config from './config'
 // import {Router}  from "aurelia-router";
 // import {PLATFORM} from 'aurelia-pal';
 // import config from './config';
@@ -9,22 +11,73 @@ import Socket from './services/socket';
 
 @inject(Socket, EventAggregator)
 export class Welcome {
+  interventions = []
+  violations = []
+  itemTaken = undefined
+
   constructor(socket, eventAggregator) {
     this.socket = socket;
     this.eventAggregator = eventAggregator;
-    console.log('EA', eventAggregator);
   }
 
-  activate() {
-    console.log('LA', this.EventAggregator);
+  async activate() {
+    this.interventions = await axios.get(config.host + '/api/v1/intervention').then(response => response.data.slice(0,9));
+    this.violations = await axios.get(config.host + '/api/v1/violation').then(response => response.data.slice(0,9));
+
     this.eventAggregator.subscribe('msg', payload => {
       console.log('EA: msg received', payload);
       this.pongReception = true;
       setTimeout(() => { this.pongReception = false }, 5000);
     });
+
+    this.eventAggregator.subscribe('newIntervention', payload => {
+      console.log('newIntervention', payload)
+      this.interventions.push(payload)
+    });
+
+    this.eventAggregator.subscribe('newViolation', payload => {
+      console.log('--  newViolation', typeof payload, payload)
+      this.violations.push(payload)
+      if (this.violations.length > 10) {
+        this.violations.shift()
+      }
+    });
+
+    this.eventAggregator.subscribe('take-intervention-refused', payload => {
+      console.log('--  refused drone controle', typeof payload, payload)
+      this.itemTaken = undefined
+    });
+
+    this.eventAggregator.subscribe('take-intervention-accepted', payload => {
+      console.log('--  accepted drone controle', typeof payload, payload)
+      this.droneControl = true
+    });
   }
 
   actionButton() {
     this.socket.send('another-event', {username: 'camille'});
+  }
+
+  takeIntervention(intervention) {
+    console.log('interv', intervention.uuid, intervention)
+    this.itemTaken = intervention.uuid
+    this.socket.send('take-intervention', {uuid: intervention.uuid});
+  }
+
+  returnIntervention(intervention) {
+    console.log('return interv', intervention.uuid, intervention)
+    this.itemTaken = undefined
+    this.droneControl = undefined
+    this.socket.send('return-intervention', {uuid: intervention.uuid});
+  }
+
+  closeIntervention(intervention) {
+    this.itemTaken = undefined
+    this.droneControl = undefined
+    this.socket.send('close-intervention', {uuid: intervention.uuid});
+
+    // fixMe: Must wait server response
+    const idx = this.interventions.findIndex(i => i.uuid === intervention.uuid)
+    this.interventions.splice(idx, 1)
   }
 }
